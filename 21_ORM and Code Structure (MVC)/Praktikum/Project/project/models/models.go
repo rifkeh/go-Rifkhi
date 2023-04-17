29bb67d7f0c0e3a33385ecba0c83f7a65b68d759
Rifkhi Akbar <rifkhihebat@upi.edu>
2023-04-17 11:20:16 +0700

    added some screenshoot

--- 21_ORM and Code Structure (MVC)/Praktikum/Project/project/models/models.go
+++ 21_ORM and Code Structure (MVC)/Praktikum/Project/project/models/models.go
@@ -9,7 +9,6 @@ type User struct {
 	Name     string `json:"name" form:"name"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
-	Token    string `json:"token" form:"token"`
 	Blogs    []Blog `gorm:"foreignkey:UserId"`
 }
 
